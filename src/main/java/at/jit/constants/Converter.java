cb5e18b18d21fdb76bde16cfdf234ac183c50ea1
M. Yoga Nugraha <myoganugraha@gmail.com>
2024-09-23 22:46:32 +0700

    chore: add convert constant

--- src/main/java/at/jit/constants/Converter.java
+++ src/main/java/at/jit/constants/Converter.java
@@ -12,4 +12,9 @@ public class Converter {
     public static final String DMN_ID = "someId";
     public static final String DMN_NAME = "someName";
     public static final String DECISION_TABLE_ID = "decisionTable";
+    public static final String EXECUTION_PLATFORM = "Camunda Cloud";
+    public static final String EXECUTION_PLATFORM_VERSION = "8.5.0";
+    public static final String EXECUTION_PLATFORM_ATTRIBUTE_NAME = "modeler:executionPlatform";
+    public static final String EXECUTION_PLATFORM_VERSION_ATTRIBUTE_NAME = "modeler:executionPlatformVersion";
+    public static final String MODELER_NS = "http://camunda.org/schema/modeler/1.0";
 }
